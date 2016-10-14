e3081ee55c1f07f7176ea68bf2b774713d5296a2
SimMog <simimog@gmail.com>
2016-10-14 12:28:32 +0200

    first commit

---
+++ app/src/main/java/com/example/dikiikrot/laprimaprova/Test.java
@@ -0,0 +1,9 @@
+package com.example.dikiikrot.laprimaprova;
+
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Ciao Cesco!");
+        //commenti a caso
+    }
+}
